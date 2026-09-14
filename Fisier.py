3c1d71d891e44eee824c1c47ad5f165046a52fc1
TurcuDaniel <turcudaniel487@gmail.com>
2026-09-14 19:59:07 +0300

    Create Fisier.py

---
+++ Fisier.py
@@ -0,0 +1,26 @@
+L = 13
+l = 8
+Aria = L*l
+Perimetru = 2 * (L+l)
+print(Perimetru); print(Aria)
+
+n = 10
+este_par = (n % 2 == 0)
+print(este_par)
+True
+
+varsta = 17
+are_bilet = True
+acces_la_cinema = varsta >= 16 and are_bilet
+print(acces_la_cinema)
+True
+
+#90 minute = 1 ora si 35 minute
+95//60 = 1
+95 % 60 = 35
+
+(2+3)*4
+
+(10-4)/2
+
+2**(3+1)
