40e27913ba2aa3d5b8499ad321267bc776bba709
TurcuDaniel <turcudaniel487@gmail.com>
2026-09-15 10:14:26 +0300

    Add files via upload

---
+++ Problema 2 15-09-2026.py
@@ -0,0 +1,9 @@
+
+a=5
+b=3
+print('Suma',a, '+', b, '=', a+b, end='\n')
+print('Diferenta',a, '-', b, '=', a-b, end='\n')
+print('Catul',a, '//', b, '=', a//b, end='\n')
+print('Restul',a, '%', b, '=', a%b, end='\n')
+print('Produsul',a, '*', b, '=', a*b, end='\n')
+print('Puterea',a, '**', b, '=', a**b, end='\n')
