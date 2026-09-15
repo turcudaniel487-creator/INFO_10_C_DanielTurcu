79bef99826fa39c0ff170c3c1cdffc8c232e3982
TurcuDaniel <turcudaniel487@gmail.com>
2026-09-15 10:13:14 +0300

    Add files via upload

---
+++ Functia print 15-09-2026.py
@@ -0,0 +1,5 @@
+print('Struguri', '100', 'kg', sep='   ', end='\n')
+print('Mere', '10', 'tone', sep='   ', end='\n')
+print('Cartofi', '50', 'kg', sep='   ', end='\n')
+print('Varza', '1000', 'q', sep='   ')
+
