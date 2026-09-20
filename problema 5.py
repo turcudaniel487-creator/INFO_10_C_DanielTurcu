f829c32917d31ed37ac3bd14a2ab15ad397f898e
TurcuDaniel <turcudaniel487@gmail.com>
2026-09-20 09:58:36 +0300

    Add files via upload

---
+++ problema 5.py
@@ -0,0 +1,15 @@
+x = False
+y = False
+print(x, y, x and y, x or y, not x, not y)
+
+x = False
+y = True
+print(x, y, x and y, x or y, not x, not y)
+
+x = True
+y = False
+print(x, y, x and y, x or y, not x, not y)
+
+x = True
+y = True
+print(x, y, x and y, x or y, not x, not y)
