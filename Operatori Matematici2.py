74e9176506d86f2b1c0f36d31bdac89df0117687
TurcuDaniel <turcudaniel487@gmail.com>
2026-09-22 09:59:25 +0300

    Add files via upload

---
+++ Operatori Matematici2.py
@@ -0,0 +1,6 @@
+lungime = float(input('lungime ='))
+latime = float(input('latime ='))
+aria = lungime * latime
+perimetru = 2 * (lungime + latime)
+print('Aria dreptunghiului =', round(aria, 2))
+print('Perimetrul dreptunghiului =', round(perimetru, 2))
